354f038de334a38a977427659390b90e232f0602
mohamedhamza0 <140764069+mohamedhamza0@users.noreply.github.com>
2023-09-08 00:57:08 +0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,20 @@
+$(document).ready(function(){
+
+$('.honestarparg').hide();
+$('.fourfivestar').click(function(){
+	$('.honestarparg').toggle();
+});
+
+// $('.javamibile').hide();
+// $('.mobilejava').click(function(){
+// 	$('.javamibile').toggle();
+// });
+
+$('.poneone').hide();
+$('.honeone').click(function(){
+	$('.poneone').toggle();
+});
+
+$('img').attr('alt','Go Daddy');
+	$('img').attr('title','Go Daddy');
+});
